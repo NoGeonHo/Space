396f6b2099d9c00df311ad698d5b79b3e4e9c14e
geonho No <embrace66@naver.com>
2020-08-14 20:01:49 +0900

    Initial Commit

--- space.js
+++ space.js
@@ -1,14 +1,44 @@
+const body = document.querySelector("body"),
+  audioE = body.querySelector("audio");
+
 const loginForm = document.querySelector(".login_form"),
   input = loginForm.querySelector("input");
 
-const checklist = document.querySelector(".checkList"),
-  check = checklist.querySelector(".check"),
+const checkList = document.querySelector(".checkList"),
+  check = checkList.querySelector(".check"),
   h1 = check.querySelector("h1");
 
 const nameList = "name";
 const show = "showing";
 const hidden = "hidden";
 const mainCheck = "user";
+const suc = "success";
+const can = "cancel";
+const reName = "reName";
+
+function paintImage() {
+  const image = new Image();
+  image.src = `Images/space.gif`;
+  body.appendChild(image);
+  image.classList.add("img");
+
+  const spaceLogo = document.createElement("h1");
+  body.appendChild(spaceLogo);
+  spaceLogo.innerText = "Space";
+  spaceLogo.classList.add("space_logo");
+}
+
+function startAudio() {
+  const audioFile = new Audio();
+  audioFile.src = "./audio/Space.wav";
+  console.log(audioFile);
+
+  setTimeout(function () {
+    audioFile.play();
+    audioFile.currentTime = 0;
+    audioFile.loop = true;
+  }, 3000);
+}
 
 function saveName(nameUser) {
   localStorage.setItem(nameList, nameUser);
@@ -19,11 +49,36 @@ function createName(event) {
   loginForm.classList.remove(hidden);
   const strName = input.value;
   saveName(strName);
+  loginForm.classList.remove(show);
+  loginForm.classList.add(suc);
+  paintImage();
+  startAudio();
 }
 
-function yesName() {}
+function createReName(event) {
+  event.preventDefault();
+  loginForm.classList.remove(hidden);
+  const strName = input.value;
+  saveName(strName);
+  loginForm.classList.remove(reName);
+  loginForm.classList.add(suc);
+  paintImage();
+  startAudio();
+}
 
-function noName() {}
+function yesName() {
+  checkList.classList.add(suc);
+  paintImage();
+  startAudio();
+}
+
+function noName() {
+  checkList.classList.add(can);
+  loginForm.classList.add(reName);
+  loginForm.classList.remove(show);
+  loginForm.classList.remove(hidden);
+  handleReName();
+}
 
 function paintName(text) {
   const btn1 = document.createElement("button");
@@ -43,6 +98,10 @@ function paintName(text) {
 
   h1.innerText = `your name is ${text}?`;
   loginForm.classList.add(hidden);
+}
+
+function handleReName() {
+  loginForm.addEventListener("submit", createReName);
 }
 
 function handleName() {
